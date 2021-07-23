bebbe463bab56bec42e5bc12aa6ee3541bec3674
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-24 03:36:10 +0900

    add ユーザバリデーションを新しく作成した．

--- cmd/interfaces/validators/user/user_validator.go
+++ cmd/interfaces/validators/user/user_validator.go
@@ -1,1 +1,7 @@
-package user
+package validators
+
+type UserValidator struct {
+	UserId         int    `json:"id" binding:"required"`
+	UserName       string `json:"name" binding:"required"`
+	UserGenderType int    `json:"gender_type" binding:"required"`
+}
