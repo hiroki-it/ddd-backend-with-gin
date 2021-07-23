f21986e8c052651275c3d1bb114299fa1952b2e3
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-24 03:33:02 +0900

    add ユーザ構造体を新しく作成した．

--- cmd/domain/user/entities/user.go
+++ cmd/domain/user/entities/user.go
@@ -1,1 +1,12 @@
-package user
+package entities
+
+import (
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/ids"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/values"
+)
+
+type User struct {
+	id         ids.UserId
+	name       values.UserName
+	genderType values.UserGenderType
+}
