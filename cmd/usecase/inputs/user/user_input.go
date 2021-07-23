b02e1ac3b3b19c41292514ad5438a071ab738317
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-24 03:36:24 +0900

    add ユーザインプットを新しく作成した．

--- cmd/usecase/inputs/user/user_input.go
+++ cmd/usecase/inputs/user/user_input.go
@@ -1,1 +1,17 @@
-package user
+package inputs
+
+type UserInput struct {
+	userId         int
+	userName       string
+	userGenderType int
+}
+
+// NewUserInput コンストラクタ
+func NewUserInput(userId int, userName string, userGenderType int) *UserInput {
+
+	return &UserInput{
+		userId:         userId,
+		userName:       userName,
+		userGenderType: userGenderType,
+	}
+}
