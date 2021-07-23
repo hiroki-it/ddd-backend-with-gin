d9a243f55262e39bc2237b68f638a955023c3cd3
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-24 03:31:54 +0900

    add ユーザリポジトリを新しく実装した．

--- cmd/infrastructure/repositories/user/users_repository.go
+++ cmd/infrastructure/repositories/user/users_repository.go
@@ -1,1 +1,18 @@
 package user
+
+import (
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/repositories"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/infrastructure"
+)
+
+type UserRepository struct {
+	db *infrastructure.DB
+}
+
+// NewUserRepository コンストラクタ
+func NewUserRepository(db *infrastructure.DB) repositories.UserRepository {
+
+	return &UserRepository{
+		db: db,
+	}
+}
