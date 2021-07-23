f5ba347842fd15575f001942ef038752bde01ef4
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-24 03:32:20 +0900

    add ユーザユースケースを新しく実装した．

--- cmd/usecase/usecases/user/user_usecase.go
+++ cmd/usecase/usecases/user/user_usecase.go
@@ -1,1 +1,24 @@
-package user
+package usecases
+
+import (
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/entities"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/ids"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/repositories"
+)
+
+type UserUsecase struct {
+	userRepository repositories.UserRepository
+}
+
+// NewUserUsecase コンストラクタ
+func NewUserUsecase(userRepository repositories.UserRepository) *UserUsecase {
+
+	return &UserUsecase{
+		userRepository: userRepository,
+	}
+}
+
+// GetUser ユーザを取得します．
+func (uu *UserUsecase) GetUser(userId ids.UserId) (*entities.User, error) {
+	return nil, nil
+}
