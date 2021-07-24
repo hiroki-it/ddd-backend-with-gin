fc174c7412b0c8d996885273e485540be66e611f
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-25 00:26:49 +0900

    update 全ての属性の等価性を検証できるようにした．

--- cmd/domain/user/values/user_name.go
+++ cmd/domain/user/values/user_name.go
@@ -21,6 +21,21 @@ func NewUserName(firstName string, lastName string, firstKanaName string, lastKa
 	}
 }
 
+// Equal 等価性を検証します．
+func (un *UserName) Equal(target domain.Value) bool {
+	t := target.(*UserName)
+
+	// 全ての属性が一致した場合
+	if un.firstName == t.firstName &&
+		un.lastName == t.lastName &&
+		un.firstKanaName == t.firstKanaName &&
+		un.lastKanaName == t.lastKanaName {
+		return true
+	}
+
+	return false
+}
+
 // FullName フルネームを返却します．
 func (un *UserName) FullName() string {
 	return un.firstName + un.lastName
