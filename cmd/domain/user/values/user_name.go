10b401321bf462d96e828c99f310b4ec5d8d80fb
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-24 23:53:47 +0900

    add 値オブジェクトとして，ユーザ名構造体を新しく実装した．

--- cmd/domain/user/values/user_name.go
+++ cmd/domain/user/values/user_name.go
@@ -1,3 +1,29 @@
 package values
 
-type UserName string
+type UserName struct {
+	firstName     string
+	lastName      string
+	firstKanaName string
+	lastKanaName  string
+}
+
+// NewUserName コンストラクタ
+func NewUserName(firstName string, lastName string, firstKanaName string, lastKanaName string) *UserName {
+
+	return &UserName{
+		firstName:     firstName,
+		lastName:      lastName,
+		firstKanaName: firstKanaName,
+		lastKanaName:  lastKanaName,
+	}
+}
+
+// FullName フルネームを返却します．
+func (un *UserName) FullName() string {
+	return un.firstName + un.lastName
+}
+
+// FullKanaName フルネームを返却します．
+func (un *UserName) FullKanaName() string {
+	return un.firstKanaName + un.lastKanaName
+}
