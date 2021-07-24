576467a8859a86903544e67fe62619c64a78e1f6
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-24 20:41:43 +0900

    update ユーザ構造体にメソッドを追加した．

--- cmd/domain/user/entities/user.go
+++ cmd/domain/user/entities/user.go
@@ -10,3 +10,23 @@ type User struct {
 	name       values.UserName
 	genderType values.UserGenderType
 }
+
+// NewUser コンストラクタ
+func NewUser(userId ids.UserId, userName values.UserName, userGenderType values.UserGenderType) *User {
+
+	return &User{
+		id:         userId,
+		name:       userName,
+		genderType: userGenderType,
+	}
+}
+
+// Id idを返却します．
+func (u *User) Id() ids.UserId {
+	return u.id
+}
+
+// Equals 等価性を検証します．
+func (u *User) Equals(target User) bool {
+	return u.id == target.Id()
+}
