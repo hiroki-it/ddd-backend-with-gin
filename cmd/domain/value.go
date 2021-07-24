d37ebb8e3dbebeb53e924e9694729a4e8a2a463b
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-25 00:02:15 +0900

    update ファイル名を変更した．

---
+++ cmd/domain/value.go
@@ -0,0 +1,6 @@
+package domain
+
+type Value interface {
+	// Equal 等価性を検証します．
+	Equal(target Entity) bool
+}
--- cmd/domain/value_object.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type ValueObject interface {
-
-	// Equals 等価性を検証します．
-	Equals(target ValueObject) bool
-}
