110c1a535fe12ef8025f72695a0d385f5f83ef4f
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-25 02:20:58 +0900

    update 最小値1でバリデーションできるようにした．

--- cmd/interfaces/validators/user/user_validator.go
+++ cmd/interfaces/validators/user/user_validator.go
@@ -1,7 +1,7 @@
 package validators
 
 type UserValidator struct {
-	UserId         int    `json:"id" binding:"required"`
+	UserId         int    `json:"id" binding:"required,min=1"`
 	UserName       string `json:"name" binding:"required"`
-	UserGenderType int    `json:"gender_type" binding:"required"`
+	UserGenderType int    `json:"gender_type" binding:"required,min=1"`
 }
