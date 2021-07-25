68ff796c70a6924f16a43cb1a75d2a14438ea1c5
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-26 02:17:58 +0900

    update アノテーションを追加した．

--- cmd/infrastructure/middlewares/converters/id_converter.go
+++ cmd/infrastructure/middlewares/converters/id_converter.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ConvertId パスパラメータのidのデータ型を変換します．
 func ConvertId() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		id, err := strconv.Atoi(ctx.Param("id"))
