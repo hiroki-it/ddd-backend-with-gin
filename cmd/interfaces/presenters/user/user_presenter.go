a4b5bbd3a167bc3c36c5e63ead322abe227a97ae
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-28 09:59:20 +0900

    add ユースケース層のユーザドメインに関する処理結果を正常系レスポンスに変換できるようにした．

---
+++ cmd/interfaces/presenters/user/user_presenter.go
@@ -0,0 +1,38 @@
+package presenters
+
+import (
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/entities"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/interfaces/presenters"
+)
+
+type getUserPresenter struct {
+	UserId int `json:"id"`
+}
+
+type createUserPresenter struct {
+	UserId         int    `json:"id"`
+	UserName       string `json:"name"`
+	UserGenderType int    `json:"gender_type"`
+}
+
+type updateUserPresenter struct {
+	UserId         int    `json:"id"`
+	UserName       string `json:"name"`
+	UserGenderType int    `json:"gender_type"`
+}
+
+type deleteUserPresenter struct {
+	UserId int `json:"id"`
+}
+
+// ToGetUserPresenter 変換します．
+func ToGetUserPresenter(user *entities.User) *getUserPresenter {
+	return &getUserPresenter{
+		UserId: int(user.Id()),
+	}
+}
+
+var _, _, _, _ presenters.Presenter = &getUserPresenter{},
+	&createUserPresenter{},
+	&updateUserPresenter{},
+	&deleteUserPresenter{}
