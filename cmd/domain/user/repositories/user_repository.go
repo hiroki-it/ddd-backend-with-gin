6289d153f26bfa4b476398ae4e191cecd888cf28
hiroki-it <hasegawafeedshop@gmail.com>
2021-07-31 22:35:40 +0900

    update インフラ層のリポジトリに実装を強制させるメソッドを定義した．

--- cmd/domain/user/repositories/user_repository.go
+++ cmd/domain/user/repositories/user_repository.go
@@ -1,4 +1,11 @@
 package repositories
 
+import (
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/entities"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/ids"
+)
+
 type UserRepository interface {
+	FindById(id ids.UserId) (*entities.User, error)
+	FindAll() (entities.Users, error)
 }
