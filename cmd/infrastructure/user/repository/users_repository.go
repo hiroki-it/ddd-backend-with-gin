497fa8cedae584077c1d6a4fee2e017f68a7c8c1
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-01 00:21:15 +0900

    update DBからユーザを取得できるようにした．

--- cmd/infrastructure/user/repository/users_repository.go
+++ cmd/infrastructure/user/repository/users_repository.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/entities"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/ids"
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/repositories"
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/infrastructure"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/infrastructure/user/dto"
 )
 
 type UserRepository struct {
@@ -16,4 +19,30 @@ func NewUserRepository(db *infrastructure.DB) repositories.UserRepository {
 	return &UserRepository{
 		db: db,
 	}
+}
+
+// FindById IDを元にユーザを返却します．
+func (ur *UserRepository) FindById(id ids.UserId) (*entities.User, error) {
+	userDTO := &dto.UserDTO{}
+
+	err := ur.db.Conn().First(userDTO, id.ToPrimitive()).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return userDTO.ToUser(), nil
+}
+
+// FindAll 全てのユーザを返却します．
+func (ur *UserRepository) FindAll() (entities.Users, error) {
+	usersDTO := &dto.UsersDTO{}
+
+	err := ur.db.Conn().First(usersDTO).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return usersDTO.ToUsers(), nil
 }
