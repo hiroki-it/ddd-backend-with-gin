f046d7391457e54618a25262c57ea1cdc38483bc
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-05 08:37:12 +0900

    update ユースケース層からインターフェース層に渡す時，プレゼンター型として返却するようにした．

--- cmd/usecase/user/interactors/user_interactor.go
+++ cmd/usecase/user/interactors/user_interactor.go
@@ -5,7 +5,9 @@ import (
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/ids"
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/repositories"
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/values"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/interfaces/user/presenters"
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/usecase/user/inputs"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/usecase/user/outputs"
 )
 
 type UserInteractor struct {
@@ -20,17 +22,43 @@ func NewUserInteractor(userRepository repositories.UserRepository) *UserInteract
 }
 
 // GetUser ユーザを取得します．
-func (ui *UserInteractor) GetUser(input *inputs.GetUserInput) (*entities.User, error) {
-	return ui.userRepository.FindById(ids.UserId(input.UserId))
+func (ui *UserInteractor) GetUser(input *inputs.GetUserInput) (*presenters.GetUserPresenter, error) {
+	user, err := ui.userRepository.FindById(ids.UserId(input.UserId))
+
+	if err != nil {
+		return nil, err
+	}
+
+	guo := &outputs.GetUserOutput{
+		UserId:         user.Id().ToPrimitive(),
+		UserName:       user.Name().FullName(),
+		UserKanaName:   user.Name().FullKanaName(),
+		UserGenderType: user.GenderType().String(),
+	}
+
+	return presenters.ToGetUserPresenter(guo), nil
 }
 
 // CreateUser ユーザを作成します．
-func (ui *UserInteractor) CreateUser(cui *inputs.CreateUserInput) (*entities.User, error) {
+func (ui *UserInteractor) CreateUser(cui *inputs.CreateUserInput) (*presenters.CreateUserPresenter, error) {
 	user := entities.NewUser(
 		ids.UserId(cui.UserId),
 		values.NewUserName(cui.UserLastName, cui.UserFirstName, cui.UserLastKanaName, cui.UserFirstKanaName),
 		values.UserGenderType(cui.UserGenderType),
 	)
 
-	return ui.userRepository.Create(user)
+	user, err := ui.userRepository.Create(user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	cuo := &outputs.CreateUserOutput{
+		UserId:         user.Id().ToPrimitive(),
+		UserName:       user.Name().FullName(),
+		UserKanaName:   user.Name().FullKanaName(),
+		UserGenderType: user.GenderType().String(),
+	}
+
+	return presenters.ToCreateUserPresenter(cuo), nil
 }
