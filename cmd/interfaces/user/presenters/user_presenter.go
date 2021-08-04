f5d094b184efdcd8970bd359ac5f296da432fcd1
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-05 08:37:23 +0900

    update レスポンスデータを変更した．

--- cmd/interfaces/user/presenters/user_presenter.go
+++ cmd/interfaces/user/presenters/user_presenter.go
@@ -1,49 +1,55 @@
 package presenters
 
 import (
-	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/domain/user/entities"
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/interfaces"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/usecase/user/outputs"
 )
 
-type getUserPresenter struct {
+type GetUserPresenter struct {
+	UserId         int    `json:"id"`
+	UserName       string `json:"name"`
+	UserKanaName   string `json:"kana_name"`
+	UserGenderType string `json:"gender_type"`
+}
+
+type CreateUserPresenter struct {
+	UserId         int    `json:"id"`
+	UserName       string `json:"name"`
+	UserKanaName   string `json:"kana_name"`
+	UserGenderType string `json:"gender_type"`
+}
+
+type UpdateUserPresenter struct {
 	UserId         int    `json:"id"`
 	UserName       string `json:"name"`
 	UserGenderType string `json:"gender_type"`
 }
 
-type createUserPresenter struct {
-	UserId         int    `json:"id"`
-	UserName       string `json:"name"`
-	UserGenderType string `json:"gender_type"`
-}
-
-type updateUserPresenter struct {
-	UserId         int    `json:"id"`
-	UserName       string `json:"name"`
-	UserGenderType string `json:"gender_type"`
-}
-
-type deleteUserPresenter struct {
+type DeleteUserPresenter struct {
 	UserId int `json:"id"`
 }
 
 // ToGetUserPresenter 取得レスポンスデータを作成します．
-func ToGetUserPresenter(user *entities.User) *getUserPresenter {
-	return &getUserPresenter{
-		UserId: int(user.Id()),
+func ToGetUserPresenter(guo *outputs.GetUserOutput) *GetUserPresenter {
+	return &GetUserPresenter{
+		UserId:         guo.UserId,
+		UserName:       guo.UserName,
+		UserKanaName:   guo.UserKanaName,
+		UserGenderType: guo.UserGenderType,
 	}
 }
 
 // ToCreateUserPresenter 作成レスポンスデータを作成します．
-func ToCreateUserPresenter(user *entities.User) *createUserPresenter {
-	return &createUserPresenter{
-		UserId:         int(user.Id()),
-		UserName:       user.Name().FullName(),
-		UserGenderType: user.GenderType().String(),
+func ToCreateUserPresenter(cuo *outputs.CreateUserOutput) *CreateUserPresenter {
+	return &CreateUserPresenter{
+		UserId:         cuo.UserId,
+		UserName:       cuo.UserName,
+		UserKanaName:   cuo.UserKanaName,
+		UserGenderType: cuo.UserGenderType,
 	}
 }
 
-var _, _, _, _ interfaces.Presenter = &getUserPresenter{},
-	&createUserPresenter{},
-	&updateUserPresenter{},
-	&deleteUserPresenter{}
+var _, _, _, _ interfaces.Presenter = &GetUserPresenter{},
+	&CreateUserPresenter{},
+	&UpdateUserPresenter{},
+	&DeleteUserPresenter{}
