95ed13a9cefe6ad9359eef796ca8abcf1eac5d3b
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-05 10:02:02 +0900

    update インターラクターでプレゼンターを保持するようにした．

--- cmd/infrastructure/routers/user_router.go
+++ cmd/infrastructure/routers/user_router.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/infrastructure"
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/infrastructure/user/repositories"
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/interfaces/user/controllers"
+	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/interfaces/user/presenters"
 	"github.com/hiroki-it/ddd-api-with-go-gin/cmd/usecase/user/interactors"
 )
 
@@ -12,7 +13,7 @@ import (
 func UserRouter(router *gin.Engine, db *infrastructure.DB) {
 	userRouter := router.Group("/users")
 	{
-		c := controllers.NewUserController(interactors.NewUserInteractor(repositories.NewUserRepository(db)))
+		c := controllers.NewUserController(interactors.NewUserInteractor(repositories.NewUserRepository(db), &presenters.UserPresenter{}))
 		userRouter.GET("/:id", c.GetUser)
 	}
 }
