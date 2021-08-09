af62a0db7c5f33a90b0f70df0a87d93288659845
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-09 23:51:22 +0900

    update 変数名を変更した．

--- cmd/infrastructure/middlewares/handle_error_middleware.go
+++ cmd/infrastructure/middlewares/handle_error_middleware.go
@@ -4,15 +4,15 @@ import "github.com/gin-gonic/gin"
 
 // HandleError ミドルウェア処理中のエラーをハンドリングします．
 func HandleError() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.Next()
+	return func(context *gin.Context) {
+		context.Next()
 
 		// 処理中の全てのエラーを取得します．
-		errs := ctx.Errors
+		errs := context.Errors
 
 		// 処理中にエラーが発生していた場合
 		if errs != nil {
-			ctx.JSON(400, gin.H{"errors": errs.Errors()})
+			context.JSON(400, gin.H{"errors": errs.Errors()})
 			return
 		}
 
--- cmd/infrastructure/routers/health_check.go
+++ cmd/infrastructure/routers/health_check.go
@@ -6,8 +6,8 @@ import (
 
 // HealthCheckRouter ルーティングを実行します．
 func HealthCheckRouter(router *gin.Engine) {
-	router.GET("/healthcheck", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{"message": "ok"})
+	router.GET("/healthcheck", func(context *gin.Context) {
+		context.JSON(200, gin.H{"message": "ok"})
 	})
 
 	return
