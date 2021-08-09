94f738a64fd027b7e6a5009c11f299f18d82731c
hiroki-it <hasegawafeedshop@gmail.com>
2021-08-09 23:57:24 +0900

    update 複数のエラーを配列としてレスポンスできるようにした．

--- cmd/interfaces/controller.go
+++ cmd/interfaces/controller.go
@@ -8,5 +8,5 @@ type Controller struct{}
 
 // ErrorJSON 異常系レスポンスを返却します．各APIのレスポンスのデータ構造を統一します．
 func (c *Controller) ErrorJSON(context *gin.Context, status int, errors []string) {
-	context.JSON(status, ErrorsPresenter(errors))
+	context.JSON(status, gin.H{"errors": errors})
 }
