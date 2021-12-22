16cf2c7661c264be929be6e6ba6118f53afd32fc
hiroki-it <hasegawafeedshop@gmail.com>
2021-12-22 21:32:12 +0900

    fix パラメータの順番を変更した．

--- cmd/infrastructure/database/db.go
+++ cmd/infrastructure/database/db.go
@@ -20,8 +20,8 @@ type DB struct {
 func NewDB() (*DB, error) {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_DATABASE"),
